6008e6021a4eccf50e73fc99fbe441290d65c98b
bagdi <bagdi_74@mail.ru>
2015-11-27 18:15:41 +0300

    ANT example

---
+++ src/main/MainClass.java
@@ -0,0 +1,10 @@
+package main;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        // чтобы запустить ANT-скрипт, в консоли нужно прописать:
+        // Ant -f build.xml, где под build.xml имеется в виду путь к этому файлу
+    }
+}
